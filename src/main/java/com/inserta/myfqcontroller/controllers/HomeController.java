c60a58bf10d64518dc8677f1cc61632bc1b0426d
rigonomelandron <franciscorodriguezcontero@gmail.com>
2022-05-13 13:42:36 +0200

    Modificación login

--- src/main/java/com/inserta/myfqcontroller/controllers/HomeController.java
+++ src/main/java/com/inserta/myfqcontroller/controllers/HomeController.java
@@ -1,26 +1,40 @@
 package com.inserta.myfqcontroller.controllers;
 
 
+import com.inserta.myfqcontroller.models.Usuario;
+import com.inserta.myfqcontroller.repos.UsuariosRepos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpSession;
+
+
 @Controller
 public class HomeController {
-
-
+     @Autowired
+     private UsuariosRepos usuariosRepos;
     @RequestMapping("/")
     public String home() {
         return "login";
     }
 
     @PostMapping("/validar")
-    public String inicioSesion(){
+    public String inicioSesion(@RequestParam String user, @RequestParam String pass, HttpSession session){
+        Usuario usuario = usuariosRepos.findByUsuarioAndPass(user, pass);
 
+        if (usuario == null) {
 
-
-        return "home";
+            return "login";
+        } else {
+            session.setAttribute("usuario", user);
+            return "home";
+        }
     }
+
+
+
 }
+
